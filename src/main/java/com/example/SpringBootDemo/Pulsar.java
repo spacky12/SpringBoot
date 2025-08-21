4f2b67af9a406be2060c517a0c3df3cd6e5d4d1a
spacky12 <spacky12@yahoo.com>
2025-08-21 09:25:32 +0530

    init commit

---
+++ src/main/java/com/example/SpringBootDemo/Pulsar.java
@@ -0,0 +1,8 @@
+package com.example.SpringBootDemo;
+
+public class Pulsar {
+    void speed()
+    {
+        System.out.println("Top speed of pulsar is 150 kmph");
+    }
+}
--- src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
@@ -1,13 +1,19 @@
 package com.example.SpringBootDemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
+	Pulsar plsr = new Pulsar();
+	@Override
+	public void run(String... args) throws Exception {
+		plsr.speed();
+	}
 }
