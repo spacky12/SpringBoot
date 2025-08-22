65384b2b40cc92073c0b2b7fcbbdbdd1f7bddf26
spacky12 <spacky12@yahoo.com>
2025-08-22 10:07:05 +0530

    Autowired

--- src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.SpringBootDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,12 +12,8 @@ public class SpringBootDemoApplication implements CommandLineRunner {
 
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
-
+	@Autowired
 	Bike bike;
-	SpringBootDemoApplication(Bike bike)
-	{
-		this.bike=bike;
-	}
 	@Override
 	public void run(String... args) throws Exception {
 		bike.speed();
